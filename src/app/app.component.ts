aa09d8947483d1ae7ecdeafef6f5a8b64dd5b6bc
KavinAnumanthasamy <kavin.ak2002@gmail.com>
2025-01-23 23:47:47 +0530

    'updated'

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'vehicle';
+}
---
+++ src/app/carslist/carslist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carslist',
+  imports: [],
+  templateUrl: './carslist.component.html',
+  styleUrl: './carslist.component.css'
+})
+export class CarslistComponent {
+  
+
+}
